3a1ff4c9e0f07390b6a7661e0aecb5ee0b144f64
Haris Lee <haris.lee168@gmail.com>
2023-07-18 00:25:55 +0800

    useState react hook second lesson

--- src/components/StateCounter.js
+++ src/components/StateCounter.js
@@ -1,14 +1,26 @@
 import React, {useState} from 'react'
 
 const StateCounter = () => {
-  const [count, setCount] = useState(0)
+  const initialCount = 0
+  const [count, setCount] = useState(initialCount)
 
-  const countHandler = () => {
-    setCount(count + 1)
+  const countHandler = (n) => {
+    setCount(prevCount => {
+      if (n === 0) {
+        return prevCount * initialCount
+      }
+      else {
+        return prevCount + n
+      }
+    })
   }
+
   return (
     <div>
-      <button onClick={countHandler}>Count {count} times</button>
+      <div>Count {count} times </div>
+      <button onClick={() => countHandler(1)}>Add</button>
+      <button onClick={() => countHandler(-1)}>Minus</button>
+      <button onClick={() => countHandler(initialCount)}>Reset</button>
     </div>
   )
 }
